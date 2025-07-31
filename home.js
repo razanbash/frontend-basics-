0f0c39be5251c5ab20095beac0c40b4b11b7fe36
razanbash <razan.oyoy123@gmail.com>
2025-07-31 18:14:16 +0300

    my assignment files

---
+++ home.js
@@ -0,0 +1,34 @@
+var age = 25
+var userName ="ahmad"
+var student = true
+var mark = null
+var user = {name : "mohamme", age:30}
+var grads = [80, 90, 78]
+
+
+
+console.log(age)
+console.log(userName)
+console.log(student)
+console.log(mark)
+console.log(user)
+console.log(grads)
+
+
+
+var navBar = document.getElementById('navbar')
+console.log(navBr)
+navBar.style.backgroundColor = "red"
+navBar.style.borderRadius = "40px"
+
+var homeLink = document.getElementsByClassName('a')[0]
+console.log(homeLink)
+homeLink.style.color ="white"
+homeLink.style.fontSize="20px"
+homeLink.style.innerHTML = "Home Link"
+var gridItme = document.getElementsByClassName("gridItme")[1]
+console.log(gridItme)
+gridImage.src = "https://kcautoshow.com/wp-content/uploads/2017/12/Mercedes-Benz-Logo-PNG-File.png"
+
+
+
